894db721dd8dea32fc07e4c3a0a2f5b6eb95d4c2
997962520 <997962520@qq.com>
2017-02-23 11:18:08 +0800

    登录小修改

--- login.php
+++ login.php
@@ -17,7 +17,8 @@ elseif($password == "")
 }
 
 $result = $my_db->database_get("select name from user where name='$username'and password='$password'");
-if(count($result)!=0) {
+if(count($result)!=0) //用户名密码输入正确 登陆成功
+{
     session_start();
     //$_SESSION["role"] = $role;
     $_SESSION["user_name"] = $result[0]['name'];
@@ -27,10 +28,12 @@ if(count($result)!=0) {
           </form>';
 
     echo "<script type='text/javascript'>document.cookie.submit();</script>";
-    }
-    else {
-        echo "<script type='text/javascript'>alert('用户名或密码错误');location='index.html';</script>";
-    }
+    echo "<script type='text/javascript'>alert('登录完成，将进入个人中心');location='user/user.php';</script>";
+}
+else//用户名或密码错误
+{
+    echo "<script type='text/javascript'>alert('用户名或密码错误');location='index.html';</script>";
+}
 
 ?>
 </html>
