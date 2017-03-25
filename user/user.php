45a196d25743ab4402abbb31944e5449afc69081
997962520 <997962520@qq.com>
2017-03-25 11:34:51 +0800

    个人中心添加未登录不能进入

--- user/user.php
+++ user/user.php
@@ -6,6 +6,11 @@ session_start();
  * Date: 2017/1/15
  * Time: 11:09
  */
+
+
+if(!isset($_SESSION['user_name']))
+    echo "<script type='text/javascript'>alert('未登录，不能进入个人中心');location='../index.html';</script>";
+
 $html_A=<<<HTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
