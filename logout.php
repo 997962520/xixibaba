7e0af23a43e2c515a1666c7f838101778204aecf
997962520 <997962520@qq.com>
2017-03-25 11:30:47 +0800

    注销修改

--- logout.php
+++ logout.php
@@ -5,8 +5,8 @@
  * Date: 2017/3/7
  * Time: 13:59
  */
-
+session_start();
 unset($_SESSION['user_name']);
 unset($_SESSION['class_id']);
-
+session_unset();
 echo "<script type='text/javascript'>location = 'index.html'</script>";
